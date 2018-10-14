54ace6ccf5dc499e1b8132cac7dc5f60f6b3d800
malkaGreineman <malky8895@gmail.com>
2018-10-14 09:43:21 +0300

    444

--- MemoryGame-webapi+winform/MemoryGameWinform/MemoryGameWinform/Models/Game.cs
+++ MemoryGame-webapi+winform/MemoryGameWinform/MemoryGameWinform/Models/Game.cs
@@ -15,18 +15,6 @@ namespace MemoryGameWinform.Models
         public string CurrentTurn { get; set; }
         public Dictionary<string, string> CardArray { get; set; }
 
-        public Game()
-        {
-            CardArray = new Dictionary<string, string>();
-            int i = 0;
-            while (i < 9)
-            {
-                int numCard = new Random().Next(1, 100);
-                if (CardArray.Keys.Contains(numCard.ToString()))
-                    continue;
-                CardArray.Add(numCard.ToString(), null);
-                i++;
-            }
-        }
+      
     }
 }
